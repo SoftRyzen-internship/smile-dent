3a2af9f593a5b0b228e8174c162bb4123ddf64d4
egarra <garikhabuzov@gmail.com>
2023-03-21 17:38:47 +0200

    SD-4: hero add, imgs fix

--- src/assets/js/burger.js
+++ src/assets/js/burger.js
@@ -1,10 +1,13 @@
 const btn = document.querySelector('.nav__burger');
 const menu = document.querySelector('.mobile-menu');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.mobile-menu-overlay')
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('mobile-menu--opened');
     btn.classList.toggle('nav__burger-opened');
+    overlay.classList.toggle('mobile-menu-overlay--is-hidden');
+
     if (document.body.style.overflow !== 'hidden') {
         document.body.style.overflow = 'hidden';
     } else {
