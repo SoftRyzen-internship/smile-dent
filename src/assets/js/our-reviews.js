2c909e576064575dc05c5e501a2fffdc3b5e7adc
egarra <garikhabuzov@gmail.com>
2023-03-23 13:10:33 +0200

    SD-8: slider our reviews works

--- src/assets/js/our-reviews.js
+++ src/assets/js/our-reviews.js
@@ -1,11 +1,27 @@
 $('.our-reviews__slider').slick({
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     speed: 300,
     cssEase: 'ease-in-out',
     variableWidth: true,
     dots: true,
-    nextArrow: '.our-team__next',
-    prevArrow: '.our-team__prev',
+    nextArrow: '.our-reviews__next',
+    prevArrow: '.our-reviews__prev',
     infinite: false,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1279,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+    ],
   });
